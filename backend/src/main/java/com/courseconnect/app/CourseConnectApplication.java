0e0cc8dd17127c795b440e560616f78238ca925b
Tyler-L-UWO <tyler.liquornik@gmail.com>
2024-11-06 21:34:27 -0500

    commit initial be, script source fix fe

---
+++ backend/src/main/java/com/courseconnect/app/CourseConnectApplication.java
@@ -0,0 +1,13 @@
+package com.courseconnect.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseConnectApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/courseconnect/app/CourseConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.courseconnect.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
